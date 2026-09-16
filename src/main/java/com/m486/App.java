d28a7b5c0a81c186d9982821a6668c39ded0573c
DPcoder <48065653+DPlilCoDeR@users.noreply.github.com>
2026-09-16 20:25:11 +0200

    Try creacion de fixero con los datos de la persona

--- src/main/java/com/m486/App.java
+++ src/main/java/com/m486/App.java
@@ -1,10 +1,17 @@
 package com.m486;
 
+import com.m486.model.Persona;
+import com.m486.persistencia.GestioPersona;
+import com.m486.persistencia.GestioPersonaImpl;
+
+import java.io.File;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        GestioPersona gp = new GestioPersonaImpl(new File("./dades/persones"));
+        gp.afegirPersona(new Persona("John", 23));
     }
 }
